503d5b028bda8b4331f36ff850f43dac822e28d5
unknown <dangevald@gmail.com>
2022-12-07 10:17:23 +0000

    mise à jour récente sur la mise en forme

--- question.php
+++ question.php
@@ -12,10 +12,6 @@
 
 	<link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
 
-
-
-
-
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 	<!-- Favicons -->
